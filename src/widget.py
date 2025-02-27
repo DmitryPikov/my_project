2af0afe83034f475cea4c68a2d4880edf11659a8
Dmitry Pikov <dmitrypikov@yandex.ru>
2025-02-27 11:07:24 +0400

    Add a function get_date()

--- src/widget.py
+++ src/widget.py
@@ -8,3 +8,8 @@ def mask_account_card(account_card: str) -> str:
         return f"{account_card_list[0]} {get_mask_account(int(account_card_list[-1]))}"
     else:
         return f"{" ".join(account_card_list[:-1])} {get_mask_card_number(int(account_card_list[-1]))}"
+
+
+def get_date(date_time: str) -> str:
+    """Функция, которая принимает на вход строку с датой и возвращает строку в формате "ДД.ММ.ГГГГ" """
+    return f"{".".join(date_time.split('T')[0].split('-')[::-1])}"
