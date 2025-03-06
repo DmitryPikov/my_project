bb9c86241f30d9975e85217dc77771e4b5a06771
Dmitry Pikov <dmitrypikov@yandex.ru>
2025-03-07 01:47:29 +0400

    Add a function sort_by_date()

--- src/processing.py
+++ src/processing.py
@@ -5,3 +5,8 @@ def filter_by_state(list_dictionaries: list, state: str = "EXECUTED") -> list:
         if item.get("state") == state:
             list_by_state.append(item)
     return list_by_state
+
+
+def sort_by_date(list_dictionary: list, reverse: bool = True) -> list:
+    """Функция сортировки банковских операций по дате"""
+    return sorted(list_dictionary, key=lambda item: item["date"], reverse=reverse)
