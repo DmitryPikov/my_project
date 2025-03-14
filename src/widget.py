bf8c7a5319f3aa46b2f8d40bd78df23d078370ba
Dmitry Pikov <dmitrypikov@yandex.ru>
2025-03-14 11:23:50 +0400

    Fixes function mask_account_card() and get_date()

--- src/widget.py
+++ src/widget.py
@@ -7,9 +7,9 @@ def mask_account_card(account_card: str) -> str:
     if "Счет" in account_card_list:
         return f"{account_card_list[0]} {get_mask_account(int(account_card_list[-1]))}"
     else:
-        return f"{" ".join(account_card_list[:-1])} {get_mask_card_number(int(account_card_list[-1]))}"
+        return f"{' '.join(account_card_list[:-1])} {get_mask_card_number(int(account_card_list[-1]))}"
 
 
 def get_date(date_time: str) -> str:
     """Функция, которая принимает на вход строку с датой и возвращает строку в формате "ДД.ММ.ГГГГ" """
-    return f"{".".join(date_time.split('T')[0].split('-')[::-1])}"
+    return f"{'.'.join(date_time.split('T')[0].split('-')[::-1])}"
