23854cadf47e36ffab00a02c35d64edcf3eb0f48
Dmitry Pikov <dmitrypikov@yandex.ru>
2025-04-06 21:17:21 +0400

    Add logging to masks.py and utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,21 @@
+import logging
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs/masks.log', 'w')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: int) -> str:
     """Функция маскировки номера банковской карты"""
     card_number_str = str(card_number)
     if len(card_number_str) != 16 or not card_number_str.isdigit() or card_number_str == "" or card_number_str is None:
+        logger.error("Не корректный номер карты")
         raise Exception("Не корректный номер карты")
     else:
+        logger.info("Успешная маскировка банковской карты")
         return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
 
 
@@ -16,6 +28,8 @@ def get_mask_account(account_number: int) -> str:
         or account_number_str == ""
         or account_number_str is None
     ):
+        logger.error("Не корректный номер счета")
         raise Exception("Не корректный номер счета")
     else:
+        logger.info("Успешная маскировка банковского счета")
         return f"**{account_number_str[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,25 @@
 import json
+import logging
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs/utils.log', 'w')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def open_json_file(file_path: str) -> list:
     """Функция, которая принимает на вход JSON файл и возвращает список словарей"""
     try:
+        logger.debug("Открываем файл JSON")
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
         if isinstance(data, list):
+            logger.info("Возвращаем список словарей")
             return data
         else:
             return []
-    except (FileNotFoundError, json.JSONDecodeError):
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        logger.error(f"Произошла ошибка {e}")
         return []
